fe6776b31330817632738f5cc1cf75ba0e073e44
dwainejade <dwainejade@gmail.com>
2020-12-06 21:52:46 -0500

    delete post working

--- User/post-router.js
+++ User/post-router.js
@@ -1,11 +1,13 @@
 const express = require("express")
-const posts = require("./post-model")
+// const posts = require("./post-model")
+const db = require("../data/db")
 
 const router = express.Router()
 
+// GET ALL POSTS
 router.get("/posts", (req, res) => {
     console.log(req.query)
-    posts.find()
+    db.find()
         .then((posts) => {
             res.status(200).json(posts)
         })
@@ -17,6 +19,27 @@ router.get("/posts", (req, res) => {
         })
 })
 
+// GET SPECIFIC POST
+router.get("/posts/:id", (req, res) => {
+    db.findById(req.params.id)
+        .then((post) => {
+            if (post) {
+                res.json(post)
+            } else {
+                res.status(404).json({
+                    message: "The post with the specified ID does not exist."
+                })
+            }
+        })
+        .catch((error) => {
+            console.log(error)
+            res.status(500).json({
+                error: "You screwed up"
+            })
+        })
+})
+
+// CREATE NEW POST
 router.post("/posts", (req, res) => {
     if (!req.body.title || !req.body.contents) {
         return res.status(400).json({
@@ -24,40 +47,73 @@ router.post("/posts", (req, res) => {
         })
     }
 
-    posts.add(req.body)
+    db.insert(req.body)
         .then((post) => {
             res.status(201).json(post)
         })
         .catch((error) => {
             console.log(error)
             res.status(500).json({
-                message: "Error adding the post",
+                error: "There was an error while saving the post to the database."
             })
         })
 })
 
+// GET COMMENTS FOR POST BY ID
+router.get("/posts/:id/comments", (req, res) => {
+    db.findPostComments(req.params.id)
+        .then((comments) => {
+            if (comments) {
+                res.json(comments)
+            } else {
+                res.status(404).json({
+                    errorMessage: "Post not found."
+                })
+            }
+        })
+        .catch((error) => {
+            console.log(error)
+            res.status(500).json({
+                error: "Error retrieving the post",
+            })
+        })
+})
 
+// CREATE NEW COMMENT FOR POST BY ID
+router.post("/posts/:id/comments", (req, res) => {
+    if (!req.body.text) {
+        return res.status(400).json({
+            errorMessage: "Please provide text for the comment."
+        })
+    }
 
-// router.get("/posts/:id", (req, res) => {
-//     users.findById(req.params.id)
-//         .then((user) => {
-//             if (user) {
-//                 res.status(200).json(user)
-//             } else {
-//                 res.status(404).json({
-//                     message: "User not found",
-//                 })
-//             }
-//         })
-//         .catch((error) => {
-//             console.log(error)
-//             res.status(500).json({
-//                 message: "Error retrieving the user",
-//             })
-//         })
-// })
+    db.insertComment(req.body)
+        .then((comment) => {
+            res.status(201).json(comment)
+        })
+        .catch((error) => {
+            console.log(error)
+            res.status(500).json({
+                error: "Error posting comment",
+            })
+        })
+})
 
-
+// DELETE POST BY ID
+router.delete("/posts/:id", (req, res) =>{
+    db.remove(req.params.id)
+        .then((count) => {
+            if (count > 0) {
+                res.status(200).json({
+                    message: "The post has been removed",
+                })
+            } else {
+                res.status(404).json({
+                    message: "The post could not be found",
+                })
+            }
+        })
+})
 
 // router.put("/users/:id", (req, res) => {
 //     if (!req.body.name || !req.body.email) {
